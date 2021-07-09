26213e4e67bddb2afcd11bcc8812e72ca4fced4b
GoMirza <59118281+GoMirza@users.noreply.github.com>
2021-07-09 12:09:55 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+import CarListScreen from './Modules/ManageCarBrands/List/CarListScreen';
+
+import {firebaseConfig} from './config';
+import MainNavigator from './Navigation/Navigation';
+
+const App = () => {
+  return <MainNavigator></MainNavigator>;
+};
+
+export default App;
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDpdzMzr89hfvlfrfmwdXuEGwxS3U6wfGs',
+  authDomain: 'car-showroom-a24a1.firebaseapp.com',
+  projectId: 'car-showroom-a24a1',
+  storageBucket: 'car-showroom-a24a1.appspot.com',
+  messagingSenderId: '497005101198',
+  appId: '1:497005101198:web:50e83a2b8969bb1a7809df',
+};
